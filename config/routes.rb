82d8a21699f2cdc3ca997fe2a710c52d5752f9ac
RYO8F53 <89722923+RYO8F53@users.noreply.github.com>
2021-11-15 18:09:52 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   #get 'top/index'
   resources :users
   #resources :tweets
@@ -8,7 +7,4 @@ Rails.application.routes.draw do
   #get 'top/logout'
   #resources :likes
   root 'users#index'
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> d6f5e361d19fb122ec7000cd6613e40aeb6d2879
 end
